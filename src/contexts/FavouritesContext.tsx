6d03f18f13c02683b7299cd3adfa5b5fab51a9e6
Aseem Gaurav <aseemgaurav@microsoft.com>
2026-03-09 22:11:46 +0530

    Fixed linting issues.

--- src/contexts/FavouritesContext.tsx
+++ src/contexts/FavouritesContext.tsx
@@ -40,6 +40,7 @@ export function FavouritesProvider({ children, initialFavourites }: { children: 
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useFavourites() {
   const ctx = useContext(FavouritesContext)
   if (!ctx) throw new Error("useFavourites must be used within FavouritesProvider")
